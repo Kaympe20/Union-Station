0352185baf07258bc477161bace1c78ef8fc3763
Micha R. Albert <info@micha.zone>
2025-09-20 17:46:09 -0400

    changes

--- src/lib/Bar.svelte
+++ src/lib/Bar.svelte
@@ -5,9 +5,9 @@
 
 <div class="flex h-16 w-screen items-center bg-[#442f51ff] p-10">
 	<h1 class="text-3xl font-black text-white">Union Station Transit Planner</h1>
-	<div class="flex w-[25vw] justify-between font-bold m-3">
+	<div class="flex w-[25vw] justify-between font-bold ml-10">
 		{#each Object.values(views) as page}
-			<button class={page === currentView ? 'rounded-2xl bg-[#6b5780ff] p-3' : ''} onclick={() => {currentView = page}}>{page}</button>
+			<button class={page === currentView ? 'rounded-2xl bg-[#6b5780ff] p-3 cursor-pointer' : 'p-3 cursor-pointer'} onclick={() => {currentView = page}}>{page}</button>
 		{/each}
 	</div>
 </div>

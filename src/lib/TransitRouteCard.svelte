9aff5f703c0c1e01794f2d50408ba09f16e09041
Kayla Blosser <117483128+Kaympe20@users.noreply.github.com>
2025-09-20 18:30:55 -0400

    Start transitRouteCard

--- src/lib/TransitRouteCard.svelte
+++ src/lib/TransitRouteCard.svelte
@@ -1,3 +1,27 @@
+<script lang="ts">
+    const transitModes = {
+        car: "Car",
+        walk: "Walk",
+        bike: "Bike",
+        tram: "Tram",
+        metroRail: "Metro Rail",
+        passengerRail: "Passenger Rail",
+        bus: "Bus",
+        plane: "Plane",
+        transfer: "Transfer"
+    }
+
+    let { activeTransitMode } = $props();
+
+    if (!(Object.keys(transitModes).includes(activeTransitMode))) {
+        console.error("Passed transit mode is not an acceptable value.");
+    }
+</script>
+
+<style>
+    
+</style>
+
 <div>
     
 </div>

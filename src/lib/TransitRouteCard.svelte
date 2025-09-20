9c2f8ee3c7582070edce466947192618fbbdc141
Kayla Blosser <117483128+Kaympe20@users.noreply.github.com>
2025-09-20 18:58:32 -0400

    Add basic transit route card

--- src/lib/TransitRouteCard.svelte
+++ src/lib/TransitRouteCard.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount, type Component } from "svelte";
+
     const transitModes = {
         car: "Car",
         walk: "Walk",
@@ -17,12 +19,13 @@
     if (!(Object.keys(transitModes).includes(activeTransitMode))) {
         console.error("Passed transit mode is not an acceptable value.");
     }
+    let Icon: any = $state();
+    onMount(async() => { Icon = (await import(`/icons/${activeTransitMode}.svelte`)).default;})
 </script>
 
-<style>
-
-</style>
-
-<div>
-    
+<div class="rounded-lg bg-white w-64 h-24">
+    <Icon/>
 </div>
+
+<style>
+</style>

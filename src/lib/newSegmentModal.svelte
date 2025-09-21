36511116cd32c9f9868d0c6e227cd860fed55389
Kayla Blosser <117483128+Kaympe20@users.noreply.github.com>
2025-09-21 03:59:12 -0400

    finish newSegmentModal

--- src/lib/newSegmentModal.svelte
+++ src/lib/newSegmentModal.svelte
@@ -2,25 +2,32 @@
     import SveltyPicker from 'svelty-picker';
     import { transitModes } from '$lib/utils';
 
+    
     let departureTime = $state(new Date("2025-09-20T07:00:00Z").toISOString());
     let arrivalTime = $state(new Date("2025-09-20T07:00:00Z").toISOString());
 
     let cost: Number = $state(0);
+    let selectedTransitMode = $state("");
+
+    $effect(() => {
+        console.log("Departure Time:", departureTime);
+        console.log("Arrival Time:", arrivalTime);
+        console.log("Cost:", cost);
+        console.log("Selected Transit Mode:", selectedTransitMode);
+    });
 </script>
 
 <div class="bg-[#473a54] border-1 border-black w-200 h-auto rounded-2xl flex items-center flex-col gap-10 p-5 text-center">
     <div>
         <p class="mb-2 text-2xl font-bold">Departure</p>
         <div class="flex gap-5">
-            <SveltyPicker mode="date" bind:value={departureTime} pickerOnly={true} />
-            <SveltyPicker mode="time" bind:value={departureTime} pickerOnly={true} />
+            <SveltyPicker mode="datetime" format="yyyy-mm-ddThh:mm:ssZ" bind:value={departureTime} pickerOnly={true} />
         </div>
     </div>
     <div>
         <p class="mb-2 text-2xl font-bold ">Arrival</p>
         <div class="flex gap-5">
-            <SveltyPicker mode="date" bind:value={arrivalTime} pickerOnly={true} />
-            <SveltyPicker mode="time" bind:value={arrivalTime} pickerOnly={true} />
+            <SveltyPicker mode="datetime" format="yyyy-mm-ddThh:mm:ssZ" bind:value={arrivalTime} pickerOnly={true} />
         </div>
     </div>
     <div class="flex gap-3 justify-between">
@@ -30,7 +37,7 @@
         </div>
         <div class="flex-1">
             <p class="mb-2 text-2xl font-bold ">Transit Mode</p>
-            <select class="border-2 border-gray-300 rounded-md p-2 w-full h-12">
+            <select bind:value={selectedTransitMode} class="border-2 border-gray-300 rounded-md p-2 w-full h-12">
                 {#each Object.keys(transitModes) as mode}
                     <option value={mode}>{transitModes[mode as keyof typeof transitModes]}</option>
                 {/each}

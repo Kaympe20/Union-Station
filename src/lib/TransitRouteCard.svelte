f9728ba2806da2971640c6e864581be8201bc4ab
Kayla Blosser <117483128+Kaympe20@users.noreply.github.com>
2025-09-21 00:39:27 -0400

    Format TransitRouteCard.svelte

--- src/lib/TransitRouteCard.svelte
+++ src/lib/TransitRouteCard.svelte
@@ -2,13 +2,13 @@
 	import { transitModes } from '$lib/utils';
 	import { onMount } from 'svelte';
 
-    let isReady = $state(false);
+	let isReady = $state(false);
 
 	let { activeTransitMode, departure, arrival, cost } = $props();
 
 	let duration: number = $state(0);
 
-    console.log(activeTransitMode)
+	console.log(activeTransitMode);
 
 	if (!Object.keys(transitModes).includes(activeTransitMode)) {
 		console.error('Passed transit mode is not an acceptable value.');
@@ -18,32 +18,42 @@
 
 	onMount(async () => {
 		Icon = (await import(`$lib/icons/${activeTransitMode}.svelte`)).default;
-        isReady = true;
-		duration = (arrival - departure) / 1000
-		departure = String(departure.getUTCHours()).padStart(2, '0') + ":" + String(departure.getUTCMinutes()).padStart(2, '0') 
-		arrival = String(arrival.getUTCHours()).padStart(2, '0') + ":" + String(arrival.getUTCMinutes()).padStart(2, '0') 
+		isReady = true;
+		duration = (arrival - departure) / 1000;
+		departure =
+			String(departure.getUTCHours()).padStart(2, '0') +
+			':' +
+			String(departure.getUTCMinutes()).padStart(2, '0');
+		arrival =
+			String(arrival.getUTCHours()).padStart(2, '0') +
+			':' +
+			String(arrival.getUTCMinutes()).padStart(2, '0');
 	});
 </script>
 
 <div class="h-32 w-64 rounded-lg bg-white">
-    {#if isReady}
-		<div class="flex items-center w-full h-full">
-			<div class="flex items-center justify-evenly w-18 h-18 m-2 bg-purple-600 rounded-full">
+	{#if isReady}
+		<div class="flex h-full w-full items-center">
+			<div class="m-2 flex h-18 w-18 items-center justify-evenly rounded-full bg-purple-600">
 				<div class="h-10 w-10">
-					<Icon/>
+					<Icon />
 				</div>
 			</div>
-			<div class="text-black text-center w-34 font-bold text-sm">
-				<h1>Departure: { departure }</h1>
-				<h1>Duration: { String(duration / 60 / 60).padStart(2, '0') }:{ String(duration % (60*60)).padStart(2, '0') }</h1>
-				<h1>Arrival: { arrival }</h1>
-				<br/>
-				<h1>Cost: ${ cost }</h1>
+			<div class="w-34 text-center text-sm font-bold text-black">
+				<h1>Departure: {departure}</h1>
+				<h1>
+					Duration: {String(duration / 60 / 60).padStart(2, '0')}:{String(
+						duration % (60 * 60)
+					).padStart(2, '0')}
+				</h1>
+				<h1>Arrival: {arrival}</h1>
+				<br />
+				<h1>Cost: ${cost}</h1>
 			</div>
 		</div>
 	{:else}
 		<p>Wait for the damn icon you useless idiot!</p>
-    {/if}
+	{/if}
 </div>
 
 <style>
